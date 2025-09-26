6a471e4e1dd66bc91c428f2282d5936bdd9397cd
LeonardoCarbajal1 <leonardocarrbajal2022@.com>
2025-09-26 15:30:13 -0500

    comit xsdxdxd

--- codigoPrueba.py
+++ codigoPrueba.py
@@ -1,3 +1,33 @@
-print("hola")
-numero=input("ingrese numero:")
-print("holaaaaaa")
+import sys
+
+ListaPerros=[]
+class Perro:#constructor (lo de entre parentesis serian los atributos)
+  def __init__(self,nombre=None,edad=None):
+    self.nombre=nombre
+    self.edad=edad
+  def CrearPerro(self):
+    nombre=input("Ingrese el nombre del perro:")
+    edad=int(input("Ingrese la edad del perro:"))
+    perro=Perro(nombre,edad)
+    ListaPerros.append(perro)
+  def MostrarLista(self):
+    print("Lista de Perros:")
+    for perro in ListaPerros:
+      print(f"Nombre:",perro.nombre,"Edad:",perro.edad)
+
+def Menu():
+ opcion=0
+ p=Perro()
+ while opcion!=3:
+    print("MENÚ")
+    print("1. Crear Perro y agregarlo a una lista existente")
+    print("2.Mostrar todos los perros de la lista")
+    print("3.Salir")
+    opcion=int(input("Ingrese una opción:"))
+    if opcion==1:p.CrearPerro()
+    elif opcion==2:p.MostrarLista()
+    elif opcion==3:
+      sys.exit()
+    else:
+      print("Opcion incorrecta")
+Menu()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-=
+def Menu():
+    print("MENÚ")
+    print("1. Crear Perro y agregarlo a una lista existente")
+    print("2.Mostrar todos los perros de la lista")
+    print("3.Salir")
+Menu()
